9f7aec9f818f98eecba8f55361e6f2d039c68a93
Daniel Jorge Carbajal <danbusiness1022@gmail.com>
2026-03-01 16:25:13 -0600

    fix: upgrade vulnerable dependencies

---
+++ src/test/java/SanityTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class SanityTest {
+    @Test
+    public void basicSanity() {
+        assertTrue(true);
+    }
+}
